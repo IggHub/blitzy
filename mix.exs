4dc31436e2ab3256fd677b494227e2d8dc0edcf1
Iggy <igoririanto@rocketmail.com>
2018-04-27 16:58:20 -0500

    created basic handle_response using HTTPoison, Timex

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule Blitzy.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :blitzy,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger, :httpoison, :timex]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:httpoison, "~>1.0"},
+      {:timex, "~>3.1"},
+      {:tzdata, "~> 0.1.8", override: true}
+    ]
+  end
+end
