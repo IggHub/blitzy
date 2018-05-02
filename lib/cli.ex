53ad21ac81082198f826fc808543a1989cbef3e6
Iggy <igoririanto@rocketmail.com>
2018-05-02 17:42:18 -0500

    add OptionParse and relevant dependencies

--- lib/cli.ex
+++ lib/cli.ex
@@ -3,9 +3,13 @@ defmodule Blitzy.CLI do
   require Logger
 
   def main(args) do
+    Application.get_env(:blitzy, :master_node)
+      |> Node.start
+    Application.get_env(:blitzy, :slave_nodes)
+      |> Enum.each(&Node.connect(&1))
     args
-    |> parse_args
-    |> process_options
+      |> parse_args
+      |> process_options([node | Node.list])
   end
 
   defp parse_args(args) do
@@ -15,9 +19,38 @@ defmodule Blitzy.CLI do
   defp process_options(options, nodes) do
     case options do
       {[requests: n], [url], []} ->
-
+        {[requests: n], [url], []} ->
+	  do_requests(n, url, nodes)
       _ ->
         do_help
     end
+  end
+
+  defp do_help do
+    IO.puts """
+    Usage:
+    blitzy -n [requests] [url]
+
+    Options:
+    -n, [--requests]    # Number of requests
+
+    Example:
+    ./blitzy -n 100 https://facebook.com"
+    """
+    System.halt(0)
+  end
+
+  defp do_requests(n_requests, url, nodes) do
+    Logger.info "Pummeling #{url} with #{n_requests} requests"
+    total_nodes = Enum.count(nodes)
+    req_per_node = div(n_requests, total_nodes)
+    nodes
+      |> Enum.flat_map(fn node ->
+          1..req_per_node |> Enum.map(fn _ ->
+	    Task.Supervisor.async({Blitzy, TaskSupervisor, node}, Blitzy.Worker, :start, [url])
+	  end)
+	end)
+      |> Enum.map(&Task.await(&1, :infinity))
+      |> parse_results
   end
 end
